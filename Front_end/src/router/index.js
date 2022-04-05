7f16c1475fe3de7835247626879477b95e8dabfa
ธีรธัช ถวิลหวัง <ten.teeratach@gmail.com>
2022-04-06 04:11:57 +0700

    create vue-router
    create vue-router

---
+++ Front_end/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// import component
+import home from "../views/home.vue"
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: home,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
