bc05ff8cd59174648312d571e25ba1b725e3a554
ธีรธัช ถวิลหวัง <ten.teeratach@gmail.com>
2022-04-17 18:01:10 +0700

    Add mongoose to manage chat database

--- Back_end/socket/socket.js
+++ Back_end/socket/socket.js
@@ -1,4 +1,5 @@
 const socketIO = require('socket.io')
+var ChatDB = require('../models/chat')
 
 module.exports = {
     getSocket: (httpServer) => {
@@ -9,21 +10,43 @@ module.exports = {
         })
         io.on('connection', (socket) => {
             console.log(socket.id, ' connected.')
-            var tmp = [
-                { username: "นายA", comment: "ทักทาย", datetime: "Tue, 12 Apr 2022, 21:06:46" },
-                { username: "นายB", comment: "ทักทาย", datetime: "Tue, 12 Apr 2022, 21:06:46" },
-                { username: "นายC", comment: "ทักทาย", datetime: "Tue, 12 Apr 2022, 21:06:46" },
-                // { username: "นายD", comment: "ทักทาย", datetime: "Tue, 12 Apr 2022, 21:06:46" },
-                // { username: "นายE", comment: "ทักทาย", datetime: "Tue, 12 Apr 2022, 21:06:46" },
-            ]
+
             // emit to client connection
             // get data from database
-            socket.emit('allMessage', tmp)
+            ChatDB.find().exec((err, doc) => {
+                // remove old comment when data over 100 record.
+                if (doc.length > 50) {
+                    // console.log('check len ',doc.length)
+                    var i = 0;
+                    for (i = 0; i < 50; i++) {
+                        ChatDB.findByIdAndDelete(doc[i]._id, {
+                            useFindAndModify: false
+                        }).exec((err) => {
+                            if (err) console.log('Delete record error : ', err)
+                        })
+                    }
+                }
+                socket.emit('allMessage', doc)
+            })
 
             socket.on('addMessage', (data) => {
-                var temp = { username: data.username, comment: data.comment, datetime: data.datetime }
+                var temp = {
+                    username: data.username,
+                    comment: data.comment,
+                    datetime: data.datetime
+                }
                 // emit to all client except sender
                 socket.broadcast.emit('newMessage', temp)
+
+                // save to database
+                let tempDB = new ChatDB({
+                    'username': data.username,
+                    'comment': data.comment,
+                    'datetime': data.datetime
+                })
+                ChatDB.saveComment(tempDB, (err) => {
+                    if (err) console.log('saveComment to db error : ', err)
+                })
             })
 
             socket.on('typing', (data) => {
