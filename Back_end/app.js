4a18cf82efba4da12dcec4755a5067e95299e0d5
ธีรธัช ถวิลหวัง <ten.teeratach@gmail.com>
2022-04-20 15:24:28 +0700

    Update format

--- Back_end/app.js
+++ Back_end/app.js
@@ -1,16 +1,32 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
 const router = require('./route/index')
 const socket = require('./socket/socket')
+var createError = require('http-errors');
+
+const app = express()
+
+app.use(express.json());
 
 //enables cors
 app.use(cors())
-app.use(router)
 
+// use custom router
+app.use('/', router)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    next(createError(404));
+});
+
+// set port 
 app.set('port', process.env.PORT || 8081);
+// app.set('port', process.env.PORT || 35001);
+
+// starting http server
 const httpServer = app.listen(8081, () => {
     console.log('Backend Server start at', '\x1b[36m', 'http://localhost:' + app.get('port') + '/', '\x1b[0m');
 })
 
+// binding socket io
 socket.getSocket(httpServer);
