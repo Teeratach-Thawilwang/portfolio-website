fa9e02392abcef94817906bede9eeeea34d5ca7f
ธีรธัช ถวิลหวัง <ten.teeratach@gmail.com>
2022-05-07 08:07:38 +0700

    create API for post, label

--- Back_end/route/index.js
+++ Back_end/route/index.js
@@ -2,13 +2,16 @@ const express = require('express')
 const router = express.Router();
 
 // import chat model
-var accountDB = require('../models/account');
+var accountCL = require('../models/account');
+var postCL = require('../models/post');
+var labelCL = require('../models/label');
 
 // import bcrypr
 const bcrypt = require("bcrypt");
-const { route } = require('express/lib/application');
+// const { route } = require('express/lib/application');
 
 /******************* Useful function *******************/
+// These function might be move to helpers folder
 const createCookiesLogin = async (email) => {
     // set salt = 5
     const salt = await bcrypt.genSalt(5);
@@ -21,6 +24,26 @@ const checkCookiesLogin = async (email, hashEmail) => {
     const validCookiesLogin = await bcrypt.compare(email, hashEmail)
     return validCookiesLogin
 }
+
+const getpost = async (posts, labels) => {
+    const data = [];
+    for (let i = 0; i < posts.length; i++) {
+        for (let j = 0; j < labels.length; j++) {
+            if (posts[i].post_id == labels[j].post_id) {
+                const temp = {
+                    post_id: posts[i].post_id,
+                    page_name: posts[i].page_name,
+                    post_time: posts[i].post_time,
+                    labeller: labels[j].labeller,
+                    status: labels[j].status,
+                }
+                data[i] = temp;
+                break;
+            }
+        }
+    }
+    return data
+}
 /*********************** Route ************************/
 router.get('/', (req, res) => {
     res.send('Teeratach Express.js Server')
@@ -28,7 +51,7 @@ router.get('/', (req, res) => {
 
 router.post('/login', async (req, res) => {
     const body = req.body;
-    const user = await accountDB.findOne({ email: body.email });
+    const user = await accountCL.findOne({ email: body.email });
     if (user) {
         // if email is exist, check password with hash password
         const validPassword = await bcrypt.compare(body.password, user.password)
@@ -52,9 +75,9 @@ router.post('/signin', async (req, res) => {
     if (!(body.email && body.password)) {
         return res.status(400).send({ error: "Data not formatted properly" });
     }
-    const user = await accountDB.findOne({ email: body.email });
+    const user = await accountCL.findOne({ email: body.email });
     if (!user) {
-        const user = new accountDB(body);
+        const user = new accountCL(body);
         // set salt = 7
         const salt = await bcrypt.genSalt(7);
         // encrypt password
@@ -63,7 +86,7 @@ router.post('/signin', async (req, res) => {
         let HashEmail = await createCookiesLogin(user.email)
         // insert account to db.
         console.log('insert ', user)
-        accountDB.saveAccount(user, (err) => {
+        accountCL.saveAccount(user, (err) => {
             if (err) {
                 console.log('Error in /signin : ', err)
                 res.status(500).json({ error: 'Cannot create account.' })
@@ -81,11 +104,62 @@ router.post('/checkLogin', async (req, res) => {
     let valid = await checkCookiesLogin(body.email, body.hashEmail)
     if (valid) {
         // correct email and hashEmail
-        const user = await accountDB.findOne({ email: body.email });
+        const user = await accountCL.findOne({ email: body.email });
         let accData = { username: user.username, email: body.email }
         res.status(200).json({ message: 'Valid cookies', account: accData })
     } else {
         res.status(401).json({ error: 'Invalid cookies' })
+    }
+})
+
+
+router.get('/getPosts', async (req, res) => {
+    const body = req.query
+    let postCount = await postCL.count({})
+    const nSkip = (body.nPage - 1) * body.row
+    const nLimit = body.row
+    if (body.nPage >= 1 && body.row >= 1 && postCount >= body.nPage * body.row) {
+        const posts = await postCL.find({},
+            { _id: 0, page_name: 1, post_id: 1, post_time: 1 })
+            .skip(nSkip)
+            .limit(nLimit);
+        const labels = await labelCL.find({},
+            { _id: 0, post_id: 1, labeller: 1, status: 1 })
+            .skip(nSkip)
+            .limit(nLimit);
+
+        // join by post_id
+        const data = await getpost(posts, labels)
+
+        res.status(200).json({ posts: data })
+    } else {
+        res.status(401).json({ error: 'Invalid request' })
+    }
+})
+
+router.get('/posts', async (req, res) => {
+    const body = req.query
+    const posts = await postCL.findOne({ post_id: body.post_id })
+    res.status(200).json({ posts: posts })
+})
+
+router.get('/label', async (req, res) => {
+    const body = req.query
+    const labels = await labelCL.findOne({ post_id: body.post_id })
+    res.status(200).json({ labels: labels })
+})
+
+router.post('/label/update', async (req, res) => {
+    const body = req.body
+    body.status = 'done'
+    delete body._id
+    const labels = await labelCL.findOneAndUpdate({ post_id: body.post_id }, body, { new: 1 });
+    const labelsObj = labels.toObject()
+    delete labelsObj._id
+    if (JSON.stringify(labelsObj) === JSON.stringify(body)) {
+        res.status(200).json({ message: 'Update successfully.' })
+    } else {
+        res.status(500).json({ error: 'Cannot update.' })
     }
 })
 
