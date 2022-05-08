cb174f2c3618ddbb1d0124de772c7059d30a9fbb
ธีรธัช ถวิลหวัง <ten.teeratach@gmail.com>
2022-05-09 06:27:04 +0700

    use dotenv

--- Back_end/app.js
+++ Back_end/app.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const router = require('./route/index')
 const socket = require('./socket/socket')
 var createError = require('http-errors');
+const dotenv = require('dotenv').config()
 
 const app = express()
 
