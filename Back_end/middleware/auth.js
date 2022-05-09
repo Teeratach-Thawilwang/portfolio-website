f829d8fabde569b05639099876e27e29aacba513
ธีรธัช ถวิลหวัง <ten.teeratach@gmail.com>
2022-05-10 04:56:38 +0700

    add createToken, checkToken function

--- Back_end/middleware/auth.js
+++ Back_end/middleware/auth.js
@@ -9,6 +9,7 @@ const verifyToken = (req, res, next) => {
             const decoded = JWT.verify(token, config.TOKEN_KEY)
             req.user = decoded
         } catch (err) {
+            console.log(err)
             return res.status(401).send({ error: "Invalid Token" })
         }
         return next();
@@ -17,4 +18,26 @@ const verifyToken = (req, res, next) => {
     }
 }
 
+const createToken = async (user_id, expiretime = '7d') => {
+    return JWT.sign(
+        { user_id: user_id },
+        process.env.TOKEN_KEY,
+        {
+            expiresIn: expiretime
+        })
+}
+
+const checkTokenExpire = (token) => {
+    try {
+        const decoded = JWT.verify(token, config.TOKEN_KEY)
+        if(Date.now() < decoded.exp){
+            return false
+        }
+    } catch (err) {
+        return true
+    }
+}
+
 module.exports = verifyToken;
+module.exports.checkTokenExpire = checkTokenExpire;
+module.exports.createToken = createToken;
